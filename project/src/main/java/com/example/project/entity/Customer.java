a4b7df1c3b4689f6d82eca4d83ac4eb38b64cdfd
Vĩnh Hùng <tatvinhhung2004@gmail.com>
2025-06-04 14:41:38 +0700

    commit

---
+++ project/src/main/java/com/example/project/entity/Customer.java
@@ -0,0 +1,43 @@
+package com.example.project.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "Customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cusId")
+    private Long cusId;
+
+    @Column(name = "cusfullName")
+    private String cusFullName;
+
+    @Column(name = "cusGender")
+    private String cusGender;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "cusDate")
+    private LocalDate cusDate;
+
+    @Column(name = "cusEmail")
+    private String cusEmail; // <--- PHẢI LÀ camelCase
+
+    @Column(name = "cusPhone")
+    private String cusPhone;
+
+    @Column(name = "cusPassword")
+    private String cusPassword; // <--- PHẢI LÀ camelCase
+
+    @Column(name = "cusAddress")
+    private String cusAddress;
+
+    @Column(name = "cusStatus")
+    private boolean cusStatus;
+}
