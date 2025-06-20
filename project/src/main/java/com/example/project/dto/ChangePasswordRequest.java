47193febceabe48f9a922bcd94cda1ac490841c5
mewmel <fuki.dzz@gmail.com>
2025-06-20 22:28:06 +0700

    update tính năng đổi mật khẩu

---
+++ project/src/main/java/com/example/project/dto/ChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.example.project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class ChangePasswordRequest {
+    
+    private String currentPassword;
+    private String newPassword;
+
+
+}
